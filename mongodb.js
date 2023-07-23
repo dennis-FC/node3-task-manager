3d15dcc56b95cc0dd92360d4b3acb64feecc604e
dennisFC <dennisyes25@gmail.com>
2023-07-23 14:52:41 +0800

    init commit

---
+++ mongodb.js
@@ -0,0 +1,39 @@
+// CRUD create read update delete
+//D:/自學/mongodb/bin/mongod.exe --dbpath=D:/自學/mongodb-data --> run db server
+
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectId
+
+const {MongoClient, ObjectID, ObjectId} = require('mongodb')
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+//const id = new ObjectID()
+//console.log(id)
+//console.log(id.getTimestamp())
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error,client) => {
+    if(error){
+        return console.log('Unable to connect to database')
+    }
+
+    const db = client.db(databaseName)
+    
+    // db.collection('users').deleteMany({
+    //     age:27
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+   
+    db.collection('tasks').deleteOne({
+        description:'Clean the house'
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
+    })
+})
