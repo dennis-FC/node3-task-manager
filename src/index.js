acb509b59472bcbab40ddb2f120355787298d7c6
dennis_new <dennisyes25@gmail>
2023-08-09 08:45:02 +0800

    File Uploads

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ const port = process.env.PORT || 3000
 //     }
 // })
 
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -31,18 +32,3 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async() => {
-    // const task = await Task.findById('64cdfc61d8220406b42e2573')
-    // await task.populate('owner')
-    // console.log(task.owner)
-
-    // const user = await User.findById('64cdfbb1d8220406b42e256d')
-    // await user.populate('tasks')
-    // console.log(user.tasks)
-}
-
-main()
