a35101e45271be89f2e1e9045a91c5d6e0310fd4
dennis_new <dennisyes25@gmail>
2023-08-10 21:37:43 +0800

    sending emails final

--- src/routers/user.js
+++ src/routers/user.js
@@ -4,12 +4,14 @@ const User = require('../models/user')
 const auth = require('../middleware/auth')
 const multer = require('multer')
 const sharp = require('sharp')
+const {sendWelcomeEmail,sendCancelEmail} = require('../emails/account')
 
 router.post('/users', async (req,res) => {
     const user = new User(req.body)
     
     try {
         await user.save()
+        sendWelcomeEmail(user.email,user.name)
         const token = await user.generateAuthToken()
         res.status(201).send({user,token})
     } catch(e) {
@@ -85,6 +87,7 @@ router.delete('/users/me',auth, async (req,res) => {
 
     try{
         await req.user.deleteOne()
+        sendCancelEmail(req.user.email,req.user.name)
         res.send(req.user)
     }catch(e){
         res.status(500).send(e)
