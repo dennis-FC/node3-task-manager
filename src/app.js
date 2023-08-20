ee9907b1a5e1150c8858b29949558a637b5ffef9
dennisFC <dennisyes25@gmail.com>
2023-08-20 14:50:43 +0800

    frontednd coming soon

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const path = require('path')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
 
+const publicDirectoryPath = path.join(__dirname,'../public')
+
+app.use(express.static(publicDirectoryPath))
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
